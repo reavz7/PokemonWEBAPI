ce6417f060b5063a762a0d8904eca9fa9134fe02
Kamil Stępień <UL0273317@edu.uni.lodz.pl>
2024-07-18 14:04:42 +0200

    Deleted not used class "User"

--- nauka/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace nauka
-{
-    public class User
-    {
-        public string imie { get; set;}
-        public int wiek { get; set; }
-        public string nazwisko { get; set; }
-    }
-}
